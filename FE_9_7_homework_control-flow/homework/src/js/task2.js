086de210cffb2857d69e06ff689c7696fe09acd5
Ivan Shvets <37488015+Leocete@users.noreply.github.com>
2018-08-19 01:25:45 +0300

    Changed  'whileto

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -2,10 +2,7 @@
 let maxNumber = 5;
 let prizeAmount = 0;
 let possiblePrize = 10;
-let attemptsNumber = 3;
-let gameNumber = randomNumber(maxNumber)
-
-console.log(gameNumber);
+let gameNumber = randomNumber(maxNumber);
 
 let userDecision = confirm('Do you want to play a game?');
 userDecision ? game() : alert('You did not become a millionaire, but can.')
@@ -14,34 +11,32 @@ function randomNumber(maxNumber) {
     return Math.floor(Math.random() * maxNumber);
 }
 function game() {
-	while(attemptsNumber > 0) { 
+    for(let attemptsNumber = 3, currentPrize = possiblePrize; 
+            attemptsNumber > 0; 
+            attemptsNumber--, currentPrize = Math.floor(currentPrize / 2)) {
         let userNumber = +prompt(`
-        Enter a number from 0 to ${maxNumber}
-        Attempts left: ${attemptsNumber}
-        Total prize: ${prizeAmount}$
-        Possible prize on current attempt: ${possiblePrize}$;
-        `)
-        console.log(gameNumber, userNumber)
-        if(userNumber === gameNumber) { 
-			alert('Congratulation! Your prize is: ' + Math.floor(possiblePrize));            
-			if(confirm('You want to continue?')) {
-                prizeAmount = Math.floor(possiblePrize) + prizeAmount;
-                maxNumber *= 2; 
-                possiblePrize *= 3;
-                attemptsNumber = 3;
-                gameNumber = randomNumber(maxNumber);
-				return game() 
-			} else {
-				break;
-			}
-		} else {
-            possiblePrize = Math.floor(possiblePrize / 2);
-            --attemptsNumber;
-        } // end of the if 
-	} // end of the while loop
+                Enter a number from 0 to ${maxNumber}
+                Attempts left: ${attemptsNumber}
+                Total prize: ${prizeAmount}$
+                Possible prize on current attempt: ${currentPrize}$;
+                `)
+                console.log(gameNumber, userNumber)
+                if(userNumber === gameNumber) {                             
+                    if(confirm('Congratulation! Your prize is:'+ Math.floor(currentPrize) + 'You want to continue?')) {
+                        prizeAmount += currentPrize;
+                        maxNumber *= 2; 
+                        possiblePrize *= 3;
+                        attemptsNumber = 3;
+                        gameNumber = randomNumber(maxNumber);
+                        return game();
+                    } else {
+                        break;
+                    } 
+                } 
+    } // end of the for loop
 } // end of the function
 
-alert(`Thank you for a game. Your prize is: ${prizeAmount} $ `)
+
 
 
 
