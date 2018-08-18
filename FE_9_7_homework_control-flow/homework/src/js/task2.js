2277c2d7d2a71801d8490c93cd8bfc3cf77a1fc6
Ivan Shvets <37488015+Leocete@users.noreply.github.com>
2018-08-18 23:51:27 +0300

    Update task2.js

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -1,25 +1,23 @@
 
 let maxNumber = 5;
+
 let prizeAmount = 0;
 let possiblePrize = 10;
+debugger
+let attemptsNumber = 3;
 
-let gameUpdate = {
-    moneyMultiplier: 3,
-    numberMultiplier: 2,
-    attemptsNumber: 3
-}
 
 let gameNumber = randomNumber(maxNumber)
+console.log(gameNumber) // потім забрати бо це перевірка
 
 let userDecision = confirm('Do you want to play a game?');
 userDecision === true ? game() : alert ('You did not become a millionaire, but can.')
-
-
-console.log(randomNumber) // потім забрати бо це перевірка
+debugger
+ 
 
 function randomNumber(maxNumber) {
-    return Math.floor(Math.random() * ++maxNumber);
-  }
+    return Math.floor(Math.random() * maxNumber);
+}
 
 
 function game() {
@@ -27,31 +25,27 @@ function game() {
 	while(attemptsNumber > 0) { 
         let userNumber = +prompt(`
         Enter a number from 0 to ${maxNumber}
-        Attempts left: ${attemptsAmount}
+        Attempts left: ${attemptsNumber}
         Total prize: ${prizeAmount}$
-        Possible prize on current attempt: Math.floor(${possiblePrize})$;
+        Possible prize on current attempt: ${possiblePrize}$;
         `)
-        if(userNumber === userNumber) { 
-			alert('Congratulation!   Your prize is: ' + Math.floor(possiblePrize));
-			possiblePrize /= 2;
+        console.log(gameNumber, userNumber)
+        if(userNumber === gameNumber) { 
+			alert('Congratulation!   Your prize is: ' + Math.floor(possiblePrize));            
 			if(confirm('You want to continue?')) {
+                prizeAmount = Math.floor(possiblePrize) + prizeAmount;
 				maxNumber *= 2; 
-				possiblePrize *= 3; 
+                attemptsNumber = 3;
+                gameNumber = randomNumber(maxNumber);
 				return game() 
 			} else {
 				break;
 			}
-		}
-		attemptsNumber--;
-	}
-	alert('Thank you for a game. Your prize is: ' + prizeAmount);
-	if(!prizeAmount) {
-		alert('You did not become a millionaire, but can.');
-		if(confirm('Do you want to play again?')) {
-		return game();
-	} else {
-		alert('You did not become a millionaire, but can.');
-	}
+		} else {
+            possiblePrize = Math.floor(possiblePrize / 2);
+            attemptsNumber = --attemptsNumber;
+        }
+		
 	}
 }
 
