c330639bc6e215a8e42883023f8692278747187c
Ivan Shvets <37488015+Leocete@users.noreply.github.com>
2018-08-19 00:34:47 +0300

    Update task2.js

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -1,27 +1,19 @@
 
 let maxNumber = 5;
-
 let prizeAmount = 0;
 let possiblePrize = 10;
-debugger
 let attemptsNumber = 3;
-
-
 let gameNumber = randomNumber(maxNumber)
-console.log(gameNumber) // потім забрати бо це перевірка
+
+console.log(gameNumber);
 
 let userDecision = confirm('Do you want to play a game?');
-userDecision === true ? game() : alert ('You did not become a millionaire, but can.')
-debugger
- 
+userDecision ? game() : alert('You did not become a millionaire, but can.')
 
 function randomNumber(maxNumber) {
     return Math.floor(Math.random() * maxNumber);
 }
-
-
 function game() {
-
 	while(attemptsNumber > 0) { 
         let userNumber = +prompt(`
         Enter a number from 0 to ${maxNumber}
@@ -30,26 +22,26 @@ function game() {
         Possible prize on current attempt: ${possiblePrize}$;
         `)
         console.log(gameNumber, userNumber)
-        	if(userNumber === gameNumber) { 
-			alert('Congratulation!   Your prize is: ' + Math.floor(possiblePrize));            
-				if(confirm('You want to continue?')) {
-                			prizeAmount = Math.floor(possiblePrize) + prizeAmount;
-					maxNumber *= 2; 
-                			attemptsNumber = 3;
-               		 		gameNumber = randomNumber(maxNumber);
-					return game() 
-				} else {
-					break;
-				}
+        if(userNumber === gameNumber) { 
+			alert('Congratulation! Your prize is: ' + Math.floor(possiblePrize));            
+			if(confirm('You want to continue?')) {
+                prizeAmount = Math.floor(possiblePrize) + prizeAmount;
+                maxNumber *= 2; 
+                possiblePrize *= 3;
+                attemptsNumber = 3;
+                gameNumber = randomNumber(maxNumber);
+				return game() 
+			} else {
+				break;
+			}
 		} else {
-            		possiblePrize = Math.floor(possiblePrize / 2);
-            		attemptsNumber = --attemptsNumber;
-        	}
-		
-	}
+            possiblePrize = Math.floor(possiblePrize / 2);
+            --attemptsNumber;
+        } // end of the if 
+	} // end of the while loop
+} // end of the function
 
-}
-
+alert(`Thank you for a game. Your prize is: ${prizeAmount} $ `)
 
 
 
