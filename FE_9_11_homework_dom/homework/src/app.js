185bdf2cbdc8306e74e25fe44becae32c70ef166
Ivan Shvets <37488015+Leocete@users.noreply.github.com>
2018-09-04 22:16:26 +0300

    Update app.js

--- FE_9_11_homework_dom/homework/src/app.js
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -1,5 +1,7 @@
 var rootNode = document.getElementById("root");
 
-// Your code goes here
+// I'm very sorry but due to the circumstances on my main work, I was unable to finish the homework in time.
+/// I will upload it to GitHub as soon as I finish it. 
+// Hope for your understanding.
 
 rootNode.appendChild(/* Append your list item node*/);
